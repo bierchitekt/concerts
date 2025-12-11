c724c41240c0ba4d9d3c96bb98adddf4a34c0580
bierchitekt <a-badu@gmx.de>
2025-12-11 20:09:01 +0100

    add openapi

---
+++ src/main/java/com/bierchitekt/concerts/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.bierchitekt.concerts;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI defineOpenApi() {
+        Server server = new Server();
+        server.setUrl("http://localhost:8080");
+
+
+        Info information = new Info()
+                .title("Munich Concerts API")
+                .version("1.0")
+                .description("This API exposes endpoints to manage concerts.");
+        return new OpenAPI().info(information).servers(List.of(server));
+    }
+}
