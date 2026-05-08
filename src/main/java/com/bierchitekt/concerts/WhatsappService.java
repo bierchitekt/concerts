a580339fb3a7f2dce0a2093e382843775f69b78a
bierchitekt <a-badu@gmx.de>
2026-05-08 18:33:00 +0200

    starting whatsapp worker before sending message

--- src/main/java/com/bierchitekt/concerts/WhatsappService.java
+++ src/main/java/com/bierchitekt/concerts/WhatsappService.java
@@ -1,6 +1,7 @@
 package com.bierchitekt.concerts;
 
 import jakarta.validation.constraints.NotEmpty;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.JdkClientHttpRequestFactory;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestClient;
 import java.net.http.HttpClient;
 
 @Service
+@Slf4j
 public class WhatsappService {
 
     @Value("${whatsapp.apikey}")
@@ -20,23 +22,64 @@ public class WhatsappService {
     @NotEmpty
     private String url;
 
-    public void sendMessage(String channel, String text) {
+    private RestClient getRestClient() {
         HttpClient httpClient = HttpClient.newBuilder()
                 .version(HttpClient.Version.HTTP_1_1)
                 .build();
-        RestClient restClient = RestClient.builder()
+        return RestClient.builder()
                 .requestFactory(new JdkClientHttpRequestFactory(httpClient))
                 .baseUrl(url)
+                .defaultHeader("X-Api-Key", apikey)
                 .build();
+
+    }
+
+    public void sendMessage(String channel, String text) {
+
+        int retries = 0;
+        while(!workerHasStatus("WORKING") && retries < 10){
+            activateWorker();
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                log.warn("sleep not successful", e);
+            }
+            retries++;
+        }
+
         WhatsappMessage whatsappMessage = new WhatsappMessage(
                 channel, text, null, false, false, "default");
-        restClient.post()
 
+        getRestClient().post()
                 .uri("/api/sendText")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
                 .header("X-Api-Key", apikey)
                 .body(whatsappMessage)
                 .retrieve().toBodilessEntity();
+    }
+
+    public void activateWorker() {
+        if (workerHasStatus("STOPPED")) {
+            log.info("whatsapp worker was stopped. Starting now");
+            startWorker();
+        }
+    }
+
+    private void startWorker() {
+
+        getRestClient().post()
+                .uri("/api/sessions/default/start")
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve().toBodilessEntity();
+    }
+
+    private boolean workerHasStatus(String status) {
+
+        String retrieve = getRestClient().get()
+                .uri("/api/sessions/default")
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve().body(String.class);
+        return retrieve == null || retrieve.contains(status);
     }
 }
